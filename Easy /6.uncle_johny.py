38c6b4f50081036cba20fef48a2d8e4a54cbcc20
Nikhil713 <nikhilbabu213@gmail.com>
2018-12-06 19:29:09 +0530

    Add Uncle_Johney

--- Easy /6.uncle_johny.py
+++ Easy /6.uncle_johny.py
@@ -3,14 +3,9 @@ num1=0
 for i in range(tcase):
 	no=int(input())
 	numbers=input().split()
-	for j in range(no):
-		numbers[j]=int(numbers[j])
 	num=int(input())
-	for j in range(no):
-		if(j==num):
-			num1=numbers[num-1]
-	numbers.sort()
-	print(num1)
+	num1=numbers[num-1]
+	numbers.sort(key=int)
 	for j in range(no):
 		if(numbers[j]==num1):
 			print(j+1)
