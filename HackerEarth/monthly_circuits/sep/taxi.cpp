f6ad3d775c2f61f849884ab7783150857dec1797
Nikhil713 <nikhilbabu213@gmail.com>
2019-09-21 23:59:45 +0530

    Add files

---
+++ HackerEarth/monthly_circuits/sep/taxi.cpp
@@ -0,0 +1,13 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int dist,on_firstpay,on_first,on_rest,cl_speed,cl_base,cl_min,cl_dist,onpay,clpay;
+
+    scanf("%d",&dist);
+
+
+}
