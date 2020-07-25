c6baf4bd5d6c0d33e99fc343e8a1a6c627217c0e
Nikhil713 <nikhilbabu213@gmail.com>
2020-07-26 04:16:49 +0530

    convert linked list int to number

---
+++ LeetCode/1290.bintointlinkedlist.py
@@ -0,0 +1,55 @@
+# https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+
+# Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+# Return the decimal value of the number in the linked list.
+
+ 
+
+# Example 1:
+
+
+# Input: head = [1,0,1]
+# Output: 5
+# Explanation: (101) in base 2 = (5) in base 10
+# Example 2:
+
+# Input: head = [0]
+# Output: 0
+# Example 3:
+
+# Input: head = [1]
+# Output: 1
+# Example 4:
+
+# Input: head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+# Output: 18880
+# Example 5:
+
+# Input: head = [0,0]
+# Output: 0
+ 
+
+# Constraints:
+
+# The Linked List is not empty.
+# Number of nodes will not exceed 30.
+# Each node's value is either 0 or 1.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        arr = []
+        while (head != None):
+            arr.append(head.val)
+            head = head.next
+        n = 0
+        size = len(arr)
+        for i in range(size):
+            n += arr[len(arr) - i - 1] * 2**i
+        return n
