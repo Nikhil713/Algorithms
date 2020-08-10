a6717c636a75392f82fb5fbc788a969bc3f4bd62
Nikhil713 <nikhilbabu213@gmail.com>
2020-08-10 19:57:57 +0530

    Sum and product

---
+++ LeetCode/cpp/1281.sumandproduct.cpp
@@ -0,0 +1,44 @@
+// Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+ 
+
+// Example 1:
+
+// Input: n = 234
+// Output: 15 
+// Explanation: 
+// Product of digits = 2 * 3 * 4 = 24 
+// Sum of digits = 2 + 3 + 4 = 9 
+// Result = 24 - 9 = 15
+
+// Example 2:
+
+// Input: n = 4421
+// Output: 21
+// Explanation: 
+// Product of digits = 4 * 4 * 2 * 1 = 32 
+// Sum of digits = 4 + 4 + 2 + 1 = 11 
+// Result = 32 - 11 = 21
+
+ 
+
+// Constraints:
+
+//     1 <= n <= 10^5
+
+
+class Solution {
+public:
+    
+    int subtractProductAndSum(int n) {
+        string s = to_string(n);
+        int sum = 0;
+        int product = 1;
+        int temp;
+        for (auto i: s){
+            sum+= (i - 48);
+            product*= (i - 48);
+        }
+        return(product - sum);
+    }
+};
